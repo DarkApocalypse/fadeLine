749d4ab1651e74be1309b0b7aeaee83399159f58
DarkApocalypse <linio_neo@hotmail.fr>
2019-05-30 14:07:33 +0200

    Update main.js
    Gestion des lots de lignes à cacher

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 $(document).on("ready",function(){
 	var fadeDuration = 400;				//Durée en millisecondes de l'animation
 	var idleDuration = 1000;			//Durée en millisecondes du temps d'affichage
-	var ls = $("table.hiddenLines > tbody > tr");	//Sélecteur pour les éléments à afficher
-	var i = -1, t = null;
-	var show, hide;
-	console.log("ls:",ls);
-	show = function(){
-		i = (i+1) % ls.length;
-		$(ls[i]).fadeIn(fadeDuration);
-		t = setTimeout(hide, idleDuration);
+	var selectorParent = "table.hiddenLines";	//Sélecteur parent
+	var selectorLine = "tbody > tr";		//Sélecteur pour les éléments à afficher
+	$(document.body).append(
+		"<"+"style>"+
+		selectorParent + " > " + selectorLine + 
+		"{display:none;}" +
+		"<" + "/style>"
+	);
+	var parents = $(selectorParent);
+	var f = function(parent){
+		var ls = $(e).find(selectorLine);
+		var i = -1, t = null;
+		var show, hide;
+		console.log("ls:",ls);
+		show = function(){
+			i = (i+1) % ls.length;
+			$(ls[i]).fadeIn(fadeDuration);
+			t = setTimeout(hide, idleDuration);
+		};
+		hide = function(){
+			$(ls[i]).fadeOut(fadeDuration, show);
+		};
+		show();
+		
 	};
-	hide = function(){
-		$(ls[i]).fadeOut(fadeDuration, show);
+	for(var j=0; j < parents.length; j++){		//Gestion de multiples lots de lignes "cachables"
+		var e = $(parents[j]);
+		f(e);
 	};
-	show();
 });
