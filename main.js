5c502cdcb2d243acf348f2049239fdcf9faa7497
DarkApocalypse <linio_neo@hotmail.fr>
2019-05-30 13:54:18 +0200

    Update main.js
    Correction suite aux tests sur forum actif(class css uniquement sur tableau)

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 $(document).on("ready",function(){
-	var fadeDuration = 400;			//Durée en millisecondes de l'animation
-	var idleDuration = 1000;		//Durée en millisecondes du temps d'affichage
-	var ls = $(".hiddenLines span");	//Sélecteur pour les éléments à afficher
+	var fadeDuration = 400;				//Durée en millisecondes de l'animation
+	var idleDuration = 1000;			//Durée en millisecondes du temps d'affichage
+	var ls = $("table.hiddenLines > tbody > tr");	//Sélecteur pour les éléments à afficher
 	var i = -1, t = null;
 	var show, hide;
+	console.log("ls:",ls);
 	show = function(){
 		i = (i+1) % ls.length;
 		$(ls[i]).fadeIn(fadeDuration);
